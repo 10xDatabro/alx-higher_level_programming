a879e11d23b434a93694373b53f5457784e9859f
10xDatabro <gilbert945@outlook.com>
2023-01-08 11:40:27 +0100

    feat: A Python module that prints a matrix
    This module assume that the list only contains integers
    and prints a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if type(matrix) is list:
+        row_num = len(matrix)
+        col_num = len(matrix[0])
+        for i in range(row_num):
+            cur_row = matrix[i]
+            for n in range(col_num):
+                if n < col_num - 1:
+                    print("{}".format(cur_row[n]), end=" ")
+                if n == col_num - 1:
+                    print("{}".format(cur_row[n]))
+    return None
