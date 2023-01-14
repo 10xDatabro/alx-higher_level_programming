953a9ad08718ffacaa0e8c49442e3ecb7d7c5f56
10xDatabro <gilbert945@outlook.com>
2023-01-15 00:08:57 +0100

    feat: A function that replaces or adds key/value in a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dict = a_dictionary.copy()
-    if key in new_dict.keys():
-        new_dict[key] = value
+    if key in a_dictionary.keys():
+        a_dictionary[key] = value
     else:
-        new_dict[key] = new_dict.get(key, value)
-    return new_dict
+        a_dictionary[key] = a_dictionary.get(key, value)
+    return a_dictionary
